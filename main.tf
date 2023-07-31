291a150ccce5e76d30a9bef27879c36383698813
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2023-07-31 20:19:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ data "aws_iam_policy_document" "policy" {
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.policy.json
-}	
+}
 
 #############################
 # Creating Lambda function
