0ca687a5569a06281da0f4d9aaf0bcf9059196f2
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2023-07-31 20:11:42 +0530

    Create main-checkpoint.tf

---
+++ .ipynb_checkpoints/main-checkpoint.tf
@@ -0,0 +1,99 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+provider "archive" {}
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "welcome.py"
+  output_path = "welcome.zip"
+}
+##########################
+# EC2 instance
+##########################
+resource "aws_instance" "vm-web" {
+  ami           = "ami-0ded8326293d3201b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server for web"
+    Env = "dev"
+  }
+}
+##########################
+# Bucket Creation
+##########################
+
+resource "aws_s3_bucket" "my_protected_bucket" {
+  bucket = var.bucket_name
+}
+
+#############################
+# Enable bucket versioning
+#############################
+resource "aws_s3_bucket_versioning" "my_protected_bucket_versioning" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+########################
+# Disabling bucket
+# public access
+########################
+resource "aws_s3_bucket_public_access_block" "my_protected_bucket_access" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+
+  # Block public access
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+########################
+# Creating 
+# iam_policy_document JSON
+########################
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+#############################
+# Creating IAM Policy using the JSON 
+#############################
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
+}	
+
+#############################
+# Creating Lambda function
+#############################
+resource "aws_lambda_function" "lambda" {
+  function_name = "welcome"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role    = aws_iam_role.iam_for_lambda.arn
+  handler = "welcome.lambda_handler"
+  runtime = "python3.7"
+}
